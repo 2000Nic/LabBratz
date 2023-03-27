7a2f1864ff787c209779ccdc23d2da514cd8a495
Nicolas Magnussen <nicolas.magnussen@hotmail.com>
2023-03-27 14:55:33 +0200

    fix header buttons

--- LabBratz-nicegui/header.py
+++ LabBratz-nicegui/header.py
@@ -5,7 +5,7 @@ class Header:
         app.add_static_files('/', '.')
         with ui.header().classes('items-center justify-between'):
             ui.interactive_image('./static/logo.png', on_mouse=lambda: ui.open('/')).style('height: auto; width: 100px; margin-right: 60vw; cursor:pointer;')
-            ui.button('TLC', on_click=lambda: ui.open('TLC')).classes('unelevated').style('box-shadow: none !important;')
-            ui.button('QDA', on_click=lambda: ui.open('QDA'))
-            ui.button('Bakterietælling', on_click=lambda: ui.open('bakterie'))
-            ui.button('Info', on_click=lambda: ui.open('info'))
+            ui.button('TLC', on_click=lambda: ui.open('TLC')).props("flat color=white")
+            ui.button('QDA', on_click=lambda: ui.open('QDA')).props("flat color=white")
+            ui.button('Bakterietælling', on_click=lambda: ui.open('bakterie')).props("flat color=white")
+            ui.button('Info', on_click=lambda: ui.open('info')).props("flat color=white")
