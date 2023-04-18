b16566096f5cd3dd831963880d1fec13eb2f3a18
Philly <100563531+PhillyWhotg@users.noreply.github.com>
2023-04-18 12:36:17 +0200

    d

--- LabBratz-nicegui/homepage.py
+++ LabBratz-nicegui/homepage.py
@@ -7,37 +7,47 @@ class Homepage:
         ui.colors(primary='#7EC8F2')
         Header()
         self.title()
-        self.about_cards()
+        self.tlc_card()
+        self.qda_card()
+        self.bak_card()
 
     def title(self):
         with ui.column().classes('self-center'):
-            ui.label('Velkommen til LabBratz!').classes('text-4xl')
+            ui.label('Velkommen til LabBratz!').classes('text-4xl').classes('self-center')
             ui.label('Med LabBratzz kan du nemt lave alt fra databehandling af en TLC-analyse til QDAer.').classes(
                 'text-1xl')
 
-    def about_cards(self):
-            with ui.row().classes('self-center'):
-                with ui.card().tight():
-                    ui.html('<img src="./static/TLC.png" style="width: 250px; height: 250px; object-fit: contain;">')
-                    with ui.card_section():
-                        ui.label('TLC').classes('text-2xl')
-                        ui.html('<p>TLC er en metode til at bestemme koncentrationen af en blanding</p>')
+    def tlc_card(self):
+        with ui.card().classes():
+            ui.html('<img src="./static/TLC.png" style="width: 250px; height: 250px; object-fit: contain;">').classes(
+                'self-center')
+            with ui.card_section():
+                ui.label('TLC').classes('text-2xl')
+                ui.html('<p>TLC er en metode til at bestemme <br> koncentrationen af en blanding</p>').style('')
+            with ui.card_actions():
+                ui.button('Gå til TLC', on_click=lambda: ui.open('TLC')).classes('unelevated').style(
+                    'box-shadow: none !important;').props("flat")
 
-                    with ui.card_actions():
-                        ui.button('Gå til TLC', on_click=lambda: ui.open('TLC')).classes('unelevated').style('box-shadow: none !important;').props("flat")
+    def qda_card(self):
+        with ui.card():
+            ui.html(
+                '<img src="./static/RadarChart.png" style="width: 250px; height: 250px; object-fit: contain;">').classes(
+                'self-center')
+            with ui.card_section():
+                ui.label('QDA').classes('text-2xl')
+                ui.label('QDA er ').classes('text-1xl')
+            with ui.card_actions():
+                ui.button('Gå til QDA', on_click=lambda: ui.open('QDA')).classes('unelevated').style(
+                    'box-shadow: none !important;').props('flat')
 
-                with ui.card().tight():
-                    ui.html('<img src="./static/RadarChart.png" style="width: 250px; height: 250px; object-fit: contain;">')
-                    with ui.card_section():
-                        ui.label('QDA').classes('text-2xl')
-                        ui.label('QDA er en metode til at bestemme sammensætningen af en blanding.').classes('text-1xl')
-                    with ui.card_actions():
-                        ui.button('Gå til QDA', on_click=lambda: ui.open('QDA')).classes('unelevated').style('box-shadow: none !important;').props('flat')
-
-                with ui.card().tight():
-                    ui.html('<img src="./static/Bakterier.png" style="width: 250px; height: 250px; object-fit: contain;">')
-                    with ui.card_section():
-                        ui.label('Bakterietælling').classes('text-2xl')
-                        ui.label('Automatisk optælling af bakterie kolonier').classes('text-1xl')
-                    with ui.card_actions():
-                        ui.button('Gå til Bakterietælling', on_click=lambda: ui.open('bakterie')).classes('unelevated').style('box-shadow: none !important;').props('flat')
+    def bak_card(self):
+        with ui.card():
+            ui.html(
+                '<img src="./static/Bakterier.png" style="width: 250px; height: 250px; object-fit: contain;">').classes(
+                'self-center')
+            with ui.card_section():
+                ui.label('Bakterietælling').classes('text-2xl').classes('self-center')
+                ui.label('Automatisk optælling af bakterie kolonier').classes('text-1xl')
+            with ui.card_actions():
+                ui.button('Gå til Bakterietælling', on_click=lambda: ui.open('bakterie')).classes('unelevated').style(
+                    'box-shadow: none !important;').props('flat')
