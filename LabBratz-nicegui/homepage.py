81522dc125a6e43a2e3c95f9a95b56f27e764f03
Philly <100563531+PhillyWhotg@users.noreply.github.com>
2023-04-25 08:47:18 +0200

    Koden er blevet til OOP

--- LabBratz-nicegui/homepage.py
+++ LabBratz-nicegui/homepage.py
@@ -5,11 +5,15 @@ from header import Header
 class Homepage:
     def __init__(self):
         ui.colors(primary='#7EC8F2')
+        TLC = Card("TLC", "TLC.png", "<p> TLC er en metode til at bestemme <br> koncentrationen af en blanding<p>", "TLC")
+        QDA = Card("QDA", "RadarChart.png", "<p> QDA er en metode:)<p>", "QDA")
+        Bakterie = Card("Bakterietælling", "Bakterier.png", "<p> Bakterietælling:)<p>", "bakterie")
         Header()
         self.title()
-        self.tlc_card()
-        self.qda_card()
-        self.bak_card()
+        with ui.row().classes('self-center'):
+            TLC.card()
+            QDA.card()
+            Bakterie.card()
 
     def title(self):
         with ui.column().classes('self-center'):
@@ -17,37 +21,20 @@ class Homepage:
             ui.label('Med LabBratzz kan du nemt lave alt fra databehandling af en TLC-analyse til QDAer.').classes(
                 'text-1xl')
 
-    def tlc_card(self):
-        with ui.card().classes():
-            ui.html('<img src="./static/TLC.png" style="width: 250px; height: 250px; object-fit: contain;">').classes(
-                'self-center')
-            with ui.card_section():
-                ui.label('TLC').classes('text-2xl')
-                ui.html('<p>TLC er en metode til at bestemme <br> koncentrationen af en blanding</p>').style('')
-            with ui.card_actions():
-                ui.button('Gå til TLC', on_click=lambda: ui.open('TLC')).classes('unelevated').style(
-                    'box-shadow: none !important;').props("flat")
 
-    def qda_card(self):
-        with ui.card():
-            ui.html(
-                '<img src="./static/RadarChart.png" style="width: 250px; height: 250px; object-fit: contain;">').classes(
-                'self-center')
-            with ui.card_section():
-                ui.label('QDA').classes('text-2xl')
-                ui.label('QDA er ').classes('text-1xl')
-            with ui.card_actions():
-                ui.button('Gå til QDA', on_click=lambda: ui.open('QDA')).classes('unelevated').style(
-                    'box-shadow: none !important;').props('flat')
+class Card:
+    def __init__(self, title, img, tekst, link):
+        self.title = title
+        self.img = img
+        self.tekst = tekst
+        self.link = link
 
-    def bak_card(self):
+    def card(self):
         with ui.card():
-            ui.html(
-                '<img src="./static/Bakterier.png" style="width: 250px; height: 250px; object-fit: contain;">').classes(
-                'self-center')
+            ui.html(f'<img src="./static/{self.img}" style="width: 250px; height: 250px; object-fit: contain;">').classes('self-center')
             with ui.card_section():
-                ui.label('Bakterietælling').classes('text-2xl').classes('self-center')
-                ui.label('Automatisk optælling af bakterie kolonier').classes('text-1xl')
+                ui.label(self.title).classes('text-2xl')
+                ui.html(self.tekst).style('')
             with ui.card_actions():
-                ui.button('Gå til Bakterietælling', on_click=lambda: ui.open('bakterie')).classes('unelevated').style(
+                ui.button('Gå til ' + self.title, on_click=lambda: ui.open(self.link)).classes('unelevated').style(
                     'box-shadow: none !important;').props('flat')
--- LabBratz-nicegui/info.py
+++ LabBratz-nicegui/info.py
@@ -5,13 +5,17 @@ from header import Header
 class Info:
     def __init__(self):
         ui.colors(primary='#7EC8F2')
+        self.Alfred = Medlem("Alfred", "42069911", "alfred@toiletpaper.dk")
+        self.Dona = Medlem("Dona", "42069911", "Dona@toiletpaper.dk")
+        self.Nicolas = Medlem("Nicolas", "42069911", "nicolas@toiletpaper.dk")
+        self.Phillip = Medlem("Phillip", "42069911", "phillipvad@toiletpaper.dk")
+
         Header()
         with ui.column().classes('self-center'):
             self.info()
             self.team()
 
     def info(self):
-
         with ui.card().tight().classes('self-center'):
             with ui.card_section():
                 ui.label('Info om LabBratz').classes('text-4xl')
@@ -26,7 +30,6 @@ class Info:
                 ui.html('<p>QDA: </p>')
                 ui.html('<p>Bakterietælling: </p>')
 
-
             with ui.card_section():
                 ui.label('Kontakt').classes('text-2xl')
                 ui.label('Tlf: 12345678').classes('h3')
@@ -37,22 +40,21 @@ class Info:
             with ui.card_section():
                 ui.label('Teamet bag LabBratz').classes('text-4xl').classes('self-center')
             with ui.row().classes('self-center'):
-                with ui.card_section():
-                    with ui.card_section():
-                        ui.label('Alfred').classes('text-2xl')
-                        ui.label('Alfred er en idiot').classes('h3')
+                self.Alfred.info('<p> Alfred er en idiot</p>')
+                self.Dona.info('<p> Dona er en idiot</p>')
+                self.Nicolas.info('<p> Nicolas er en idiot</p>')
+                self.Phillip.info('<p> Phillip er en idiot</p>')
 
-                with ui.card_section():
-                    with ui.card_section():
-                        ui.label('Dona').classes('text-2xl')
-                        ui.label('Dona er en idiot').classes('h3')
 
-                with ui.card_section():
-                    with ui.card_section():
-                        ui.label('Nicolas').classes('text-2xl')
-                        ui.label('Nicolas er en idiot').classes('h3')
+class Medlem:
+    def __init__(self, name, tlf, mail):
+        self.name = name
+        self.tlf = tlf
+        self.mail = mail
 
-                with ui.card_section():
-                    with ui.card_section():
-                        ui.label('Phillip').classes('text-2xl')
-                        ui.label('Phillip er en idiot').classes('h3')
+    def info(self, tekst):
+        with ui.card_section():
+            ui.label(self.name).classes('text-2xl')
+            ui.label(f"Tlf: {self.tlf}").classes('h3')
+            ui.label(f"Mail: {self.mail}").classes('h3')
+            ui.html(tekst).classes('self-center')
