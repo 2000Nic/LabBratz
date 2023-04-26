16e684da8b3060b6974988bf39df053d6f1591ed
LunaStarwalker <dona.petrovadp@gmail.com>
2023-04-26 08:46:11 +0200

    added texx

--- LabBratz-nicegui/Blob_detector.py
+++ LabBratz-nicegui/Blob_detector.py
@@ -9,27 +9,27 @@ class Blob_Detector:
 
     def setup(self):
         params = cv2.SimpleBlobDetector_Params()
-        # Change thresholds
+        """# Change thresholds
         params.minThreshold = 0
         params.maxThreshold = 100
 
         # Filter by Area.
         params.filterByArea = True
-        params.minArea = 1
+        params.minArea = 10
 
         # Filter by Circularity
         params.filterByCircularity = True
-        params.minCircularity = 0.1
+        params.minCircularity = 0.0001"""
 
         # Filter by Convexity
         params.filterByConvexity = True
-        params.minConvexity = 0.1
+        params.minConvexity = 0.000000000001
 
         params.filterByColor = True
 
         # Filter by Inertia
         params.filterByInertia = True
-        params.minInertiaRatio = 0.0000000000000001
+        params.minInertiaRatio = 0.000000000000000000001
 
         # Create a detector with the parameters
         # OLD: detector = cv2.SimpleBlobDetector(params)
--- LabBratz-nicegui/tlc.py
+++ LabBratz-nicegui/tlc.py
@@ -18,10 +18,14 @@ class TLC:
         with ui.column().style("float: left; margin-top: 50px; left: 50px; width: 500px; position: absolute; display: inline-grid;"):
             ui.label("Upload TLC").style("font-size: 40px; font-weight: bold; color: #757575;")
             ui.separator()
+            ui.label("Det er en god idé er holde kameraet parallelt med TLC-pladen før billedet tages, "
+                     "samt at beskære billedet så meget som muligt før upload. "
+                     "Dog skal både startlinjen og væskefronten være helt synlige").style("font-size: 20px; color: #757575;")
+
             self.switch = ui.switch('Automatisk markering', value=True).style("margin-top: 50px; font-size: 20px; font-weight: bold; color: #757575;")
 
             Uploader(self.open_image_page)
-            ui.label("Billedet skal enten uploades som .png eller .jpg").style("font-size: 15px; font-weight: bold; color: #757575;")
+            ui.label("Billedet skal enten uploades som .png eller .jpg").style("font-size: 15px; font-weight: bold; color: #757575; margin-bottom: 50px;")
 
     def open_image_page(self):
         ui.open(tlc_page)
