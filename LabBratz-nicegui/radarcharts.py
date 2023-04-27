add57e153f6183fcb9dba0c7d6dd4b96172765a1
Karatealfred <alfredkris33@gmail.com>
2023-04-27 12:58:18 +0200

    Radarcharts Update

--- LabBratz-nicegui/radarcharts.py
+++ LabBratz-nicegui/radarcharts.py
@@ -3,19 +3,23 @@ import plotly.graph_objects as go
 class Radarchart:
     def __init__(self):
         pass
-    def make_chart(self, nam, cats, vals,infill):
+    def make_chart(self, nam, cats, vals, show_trace_name, trace_nams):
         #Determines weather the traces are filled in
-        filling = 'none'
-        if infill:
-            filling='toself'
+        #filling = 'none'
+        #if infill:
+        #    filling='toself'
         #Copies first point and adds as last point in each trace, so the trace closes
         cats.append(cats[0])
         for n in vals:
             n.append(n[0])
         #Makes the graph
         figure = go.Figure()
-        for n in vals:
-            figure.add_trace(go.Scatterpolar(r=n,theta=cats,dr=1, fill=filling))
+        if show_trace_name:
+            for i,n in enumerate(vals):
+                figure.add_trace(go.Scatterpolar(r=n,theta=cats,dr=1, showlegend=show_trace_name, name=trace_nams[i]))
+        else:
+            for i,n in enumerate(vals):
+                figure.add_trace(go.Scatterpolar(r=n,theta=cats,dr=1, showlegend=show_trace_name))
         figure.update_layout(
             title=go.layout.Title(text=nam),
             polar={'radialaxis': {'visible': True}},
@@ -24,5 +28,5 @@ class Radarchart:
         figure.update_polars(radialaxis=dict(range=[0, 15]))
         return ui.plotly(figure).style("width: 45%")
 yolo = Radarchart()
-yolo.make_chart('SUP!',['Till a mans not hot', 'Noice dude', 'Wollah wollah, ingen bollah', 'Yo mama'],[[1,2,3,4],[3,2,1,4]],False)
+yolo.make_chart('SUP!',['Till a mans not hot', 'Noice dude', 'Wollah wollah, ingen bollah', 'Yo mama'],[[1,4,10,15],[3,2,8,4]],False,False)
 ui.run()
